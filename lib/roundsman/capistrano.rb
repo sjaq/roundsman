c2783fcb5bd7614f75fcf85616173f60e438ad17
Jean Jacques Warmerdam <me@sjaq.eu>
2013-01-17 16:37:54 +0100

    update default chef version

--- lib/roundsman/capistrano.rb
+++ lib/roundsman/capistrano.rb
@@ -172,7 +172,7 @@ require 'tempfile'
 
     namespace :chef do
 
-      set_default :chef_version, "~> 0.10.8"
+      set_default :chef_version, "~> 10.18"
       set_default :cookbooks_directory, ["config/cookbooks"]
       set_default :roles_directory, ["config/roles"]
       set_default :data_bags_directory, ["config/data_bags"]
